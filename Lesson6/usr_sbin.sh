59b9d32a3a08f5b612b5483720d7a747de6faf3e
cherevko <sergey.cherevko@hotmail.com>
2022-07-07 10:28:06 +0300

    FEATURES: 6.1.2 Subject: Bug Fixing Problem: Wrong permission mask in usr_sbin.sh Solution: Set 555 mask Note:
     Changes to be committed:
    	modified:   usr_sbin.sh

--- Lesson6/usr_sbin.sh
+++ Lesson6/usr_sbin.sh
@@ -3,4 +3,4 @@ file="$HOME/usr_executable.txt"
 path="/usr/sbin"
 #all must have r+x permission.
 #in /usr/sbin only lyncs correspond such requirement
-find $path -perm -666 | sort > $file
+find $path -perm -555 | sort > $file
