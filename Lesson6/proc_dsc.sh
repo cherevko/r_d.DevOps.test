1b712213e560e0f906a1e9d5c33262107d5e37e9
cherevko <sergey.cherevko@hotmail.com>
2022-07-12 21:28:29 +0300

    FEATURE: 6.1.3 Subject: Bug Fixing Problem: Wrong argument for lsof command
     Changes to be committed:
    	modified:   ../Lesson6/proc_dsc.sh

--- Lesson6/proc_dsc.sh
+++ Lesson6/proc_dsc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 file="$HOME/file_dscr.txt"
-#print PID and file name
-lsof -p $$ | awk '{print $2,$9}' > $file
+#print PID and file name for current user
+lsof -u $(whoami) | awk '{print $2,$9}' > $file
